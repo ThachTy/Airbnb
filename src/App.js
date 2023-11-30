b553fc610feac497b690ee136974f2f0daf7bb86
Thach Ty <nguyenngocthach1506@gmail.com>
2023-11-30 13:37:01 +0700

    Create Admin Page

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
-import UserLayout from './layouts/UserLayout';
-import Test from './components/Test';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Home from './pages/Home/Home';
-
+import logo from "./logo.svg";
+import "./App.css";
+import UserLayout from "./layouts/UserLayout";
+import Test from "./components/Test";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import AdminLayout from "./layouts/AdminLayout";
+import Admin from "./pages/Admin/Admin";
+import Users from "./pages/Users/Users";
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <UserLayout>
           <Routes>
-            <Route path='' element={<Home />} />
+            <Route path="" element={<Home />} />
           </Routes>
         </UserLayout>
+
+        {/* Admin Page */}
+        <Routes>
+          <Route path="/admin" element={<AdminLayout />}>
+            <Route index element={<Admin></Admin>}></Route>
+            <Route path="users" element={<Users></Users>}></Route>
+            <Route
+              path="locations"
+              element={<div>Locations Management</div>}
+            ></Route>
+            <Route path="rooms" element={<div>Room Management</div>}></Route>
+          </Route>
+        </Routes>
       </BrowserRouter>
     </div>
   );
