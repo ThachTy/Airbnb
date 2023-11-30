bdc7ef5e3ddb603c2af28019f89c9621fbdd8df5
Khang Lien <lienchikhang777@gmail.com>
2023-11-30 17:44:17 +0700

    adjust base axios

--- src/pages/Home/ProductList/ProductList.js
+++ src/pages/Home/ProductList/ProductList.js
@@ -5,10 +5,6 @@ import { useQuery } from 'react-query';
 export default function ProductList() {
     const [products, setproducts] = useState([]);
 
-    const query = useQuery({
-        queryKey: ['queryProduct'],
-        queryFn: () => 
-    })
 
     return (
         <div className='productList__wrapper'>
