ecc7a738e5b1f27522f087aea6c1a2f2316e59fc
Khang Lien <lienchikhang777@gmail.com>
2023-11-30 17:37:57 +0700

    adjust config base axios

--- src/pages/Home/ProductList/ProductList.js
+++ src/pages/Home/ProductList/ProductList.js
@@ -1,12 +1,14 @@
 import React, { useEffect } from 'react'
 import './ProductList.scss'
+import { useQuery } from 'react-query';
 
 export default function ProductList() {
     const [products, setproducts] = useState([]);
 
-    useEffect(() => {
-
-    }, [])
+    const query = useQuery({
+        queryKey: ['queryProduct'],
+        queryFn: () => 
+    })
 
     return (
         <div className='productList__wrapper'>
--- src/services/config.js
+++ src/services/config.js
@@ -1,7 +1,7 @@
 const { default: axios } = require("axios");
 const { TOKEN_CYBERSOFT } = require("../utils/constants");
 
-export const httpUser = axios.create({
+export const http = axios.create({
     baseURL: 'https://airbnbnew.cybersoft.edu.vn/api',
     headers: {
         tokenCybersoft: TOKEN_CYBERSOFT
